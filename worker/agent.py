6f3db33aea23342fc20a1244c1d7795e4286680b
Akshit Dudeja <dudejaakshit@gmail.com>
2025-03-18 13:57:24 +0530

    .

--- worker/agent.py
+++ worker/agent.py
@@ -388,6 +388,6 @@ if __name__ == "__main__":
         WorkerOptions(
             entrypoint_fnc=entrypoint,
             prewarm_fnc=prewarm,
-            # agent_name="userology-ai"
+            agent_name="userology-ai"
         )
     )
